b0540b84ac2c84082778cb379b6f817230fbf414
MiniH <139514821+Desezutt@users.noreply.github.com>
2025-03-10 15:17:43 +0300

    Create pyp.py

---
+++ pyp.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Создаем главное окно
+root = tk.Tk()
+root.withdraw()  # Скрываем главное окно, так как оно нам не нужно
+
+# Открываем диалоговое окно с сообщением
+messagebox.showinfo("Диалоговое окно", "Привет! Это диалоговое окно.")
+
+# Завершаем работу Tkinter
+root.destroy()
