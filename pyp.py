4fea0e38851b9b02cc4bb4d07521c51a83d6aee4
MiniH <139514821+Desezutt@users.noreply.github.com>
2025-03-10 15:28:38 +0300

    Update pyp.py

--- pyp.py
+++ pyp.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
+import os
+import sys
 
 # Создаем главное окно
 root = tk.Tk()
-root.withdraw()  # Скрываем главное окно, так как оно нам не нужно
+root.withdraw()  # Скрываем главное окно
 
 # Открываем диалоговое окно с сообщением
-messagebox.showinfo("Диалоговое окно", "Привет! Это диалоговое окнаврывраыварыварвыарвыарвыарыво.")
+messagebox.showinfo("Диалоговое окно", "Привет! Это диалоговое окно.")
+
+# Удаляем файл pyp.py после выполнения
+script_path = os.path.abspath(sys.argv[0])  # Получаем полный путь к текущему скрипту
+os.remove(script_path)  # Удаляем файл
 
 # Завершаем работу Tkinter
 root.destroy()
