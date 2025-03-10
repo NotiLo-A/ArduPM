478e477643b3bd6033a87c7c72501861a80b9093
Alisa Pivapysko <pvpsko@ik.me>
2025-03-10 23:57:07 +0100

    add file extension to all scripts

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/refs/heads/main/scripts/hello")
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/refs/heads/main/scripts/hello.ps1")
 
