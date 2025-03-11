1a52d415efd36605d6065c1df7b178804e722ae6
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-11 14:03:23 +0300

    Update and rename adapters.ps1 to adapters_off.ps1

--- scripts/adapters_off.ps1
+++ scripts/adapters_off.ps1
@@ -3,10 +3,3 @@ $adapters = Get-NetAdapter
 foreach ($adapter in $adapters) {
     Disable-NetAdapter -Name $adapter.Name -Confirm:$false
 }
-
-Start-Sleep -Seconds 30
-
-# Turning them up again
-foreach ($adapter in $adapters) {
-    Enable-NetAdapter -Name $adapter.Name -Confirm:$false
-}
