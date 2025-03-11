711cc209270ab936cd192d5104d9d4b31450c71a
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-11 14:55:38 +0300

    Update adapters_off.ps1

--- scripts/adapters_off.ps1
+++ scripts/adapters_off.ps1
@@ -1,5 +1,9 @@
 # Shutting down all network adapters
-$adapters = Get-NetAdapter
+$adapters = Get-NetAdapter -Physical
 foreach ($adapter in $adapters) {
     Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+}
+Start-Sleep -Seconds 60
+foreach ($adapter in $adapters) {
+    Enable-NetAdapter -Name $adapter.Name -Confirm:$false
 }
