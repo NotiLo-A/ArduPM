4f6e205b5454d5cb479cb5625d38072482d53c60
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-11 14:07:23 +0300

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/scripts/adapters_off.ps1")
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/scripts/adapters_on.ps1")
 
