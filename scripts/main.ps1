3a5b93b17dc1a240c45b73864923f8d729af86da
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-13 10:30:55 +0300

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,2 +1,6 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe")
+#Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe")
 
+$url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe"
+$output = "$env:TEMP\hello.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+Start-Process $output
